c976c4fd8c389a8bfdfb09cd1c76e4b293bcbfdd
Abdurauf_Abdusattorov <goabdurauf@gmail.com>
2022-10-31 15:46:04 +0500

    Initial commit

---
+++ src/main/java/com/manning/readinglist/ReadingListRepository.java
@@ -0,0 +1,11 @@
+package com.manning.readinglist;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface ReadingListRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findByReader(String reader);
+}
